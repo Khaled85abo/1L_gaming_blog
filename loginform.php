17779cf331d66e768ab66e628d59946fa4be60f7
Khaled85abo <khaledabou1985@gmail.com>
2020-02-25 12:48:01 +0100

    1L

---
+++ loginform.php
@@ -0,0 +1,25 @@
+<div >
+<img class="logo" src="./images/logo.png">
+</div>
+
+<form action="">
+        <div class="form">
+            <input id="transparent" type="text" name="email" autocomplete="off" required>
+            <label for="email" class="label-name">
+            <span class="content-name">E-mail</span>
+        </label>
+        </div>
+        <div class="form">
+            <input type="password" name="password" autocomplete="off" required>
+            <label for="password" class="label-name">
+            <span class="content-name">Password</span>
+        </label>
+        </div>
+        <div class="buttons">
+        <input type="submit" value="Log In">
+        </div>
+
+        <div class="login">
+        <a href="signup.php" class="link">Sign in!</a>
+        </div>
+    </form>
---
+++ signupform.php
@@ -0,0 +1,43 @@
+<div >
+<img class="logo" src="./images/logo.png">
+</div>
+
+<form action="">
+        <div class="form">
+            <input type="text" name="firstname" autocomplete="off" required>
+            <label for="name" class="label-name">
+            <span class="content-name">Firstname</span>
+        </label>
+        </div>
+        <div class="form">
+            <input type="text" name="lastname" autocomplete="off" required>
+            <label for="address" class="label-name">
+            <span class="content-name">Lastname</span>
+        </label>
+        </div>
+        <div class="form">
+            <input type="text" name="email" autocomplete="off" required>
+            <label for="email" class="label-name">
+            <span class="content-name">E-mail</span>
+        </label>
+        </div>
+        <div class="form">
+            <input type="password" name="password" autocomplete="off" required>
+            <label for="password" class="label-name">
+            <span class="content-name">Password</span>
+        </label>
+        </div>
+        <div class="form">
+            <input type="password" name="confirm_password" autocomplete="off" required>
+            <label for="confirm_password" class="label-name">
+            <span class="content-name">Confirm password</span>
+        </label>
+        </div>
+        <div class="buttons">
+        <input type="submit" value="Sing Up">
+        </div>
+
+        <div class="login">
+        <a href="login.php" class="link"> Already a user?</a>
+        </div>
+    </form>
